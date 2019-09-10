f209a4c324fa4d54c7363e73fb09df06b2857611
Tayler Johnson <johnsontayler@me.com>
2019-09-10 14:01:20 -0300

    created controllers and views for meals orders reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'orders/show'
+  get 'orders/new'
+  get 'orders/create'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'users#index'
+
+  resources :meals, only: [ :index, :show, :new, :create ]
+
+  resources :orders, only: [ :show, :new, :create ] do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
