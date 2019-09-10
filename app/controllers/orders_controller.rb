bef5f58548e553be87f00e0250be8b9ffeb1c82f
Tayler Johnson <johnsontayler@me.com>
2019-09-10 14:15:36 -0300

    added dashboard controller and model views

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,24 @@
 class OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
   end
 
   def new
+    @order = Order.new(order_params)
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:time, :user, :meal)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'orders/show'
-  get 'orders/new'
-  get 'orders/create'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#index'
@@ -13,4 +8,6 @@ Rails.application.routes.draw do
   resources :orders, only: [ :show, :new, :create ] do
     resources :reviews, only: [ :new, :create ]
   end
+
+  resources :dashboards, only: [ :index ]
 end
