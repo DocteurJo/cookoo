8de553202c81159ca3c590fe25996b85e708362f
Joseph Scemama <j.scemama1@gmail.com>
2019-09-11 14:25:09 -0300

    Now markers work

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,11 +1,12 @@
 import mapboxgl from 'mapbox-gl';
 
 const initMapbox = () => {
+
   const mapElement = document.getElementById('map');
 
-  const fitMapToMarkers = (map, markers) => {
+  const fitMapToMarkers = (map, marker) => {
     const bounds = new mapboxgl.LngLatBounds();
-    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    bounds.extend([ marker.lng, marker.lat ]);
     map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   };
 
@@ -16,13 +17,12 @@ const initMapbox = () => {
       style: 'mapbox://styles/mapbox/streets-v10'
     });
   // [ ... ]
-  const markers = JSON.parse(mapElement.dataset.markers);
-  markers.forEach((marker) => {
+  const marker = JSON.parse(mapElement.dataset.marker);
+
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
-  });
-  fitMapToMarkers(map, markers);
+  fitMapToMarkers(map, marker);
   }
 };
 
