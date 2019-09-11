5d65599bb02e7dfb0ce474446232cf7be1bdda47
Joseph Scemama <j.scemama1@gmail.com>
2019-09-11 11:55:05 -0300

    saving order_policy

---
+++ app/policies/order_policy.rb
@@ -0,0 +1,19 @@
+class OrderPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    (@user.class == Cook)
+  end
+
+  def show?
+    (@user == @record.user)
+  end
+
+  def create?
+    true
+  end
+end
