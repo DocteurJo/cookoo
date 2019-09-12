04e2e20ff14246934c59afe91eaa1262ee0644eb
Tayler Johnson <johnsontayler@me.com>
2019-09-12 11:02:47 -0300

    added policy scopes to dashboards controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,8 @@
 class DashboardsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index ]
+
   def index
+    @orders = policy_scope(Order).all
+    @meals = policy_scope(Meal).all
   end
 end
