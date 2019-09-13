bb997b4f034eafb3f6c162a179d35f38cf0c9a7d
Tayler Johnson <johnsontayler@me.com>
2019-09-13 11:49:34 -0300

    daily meal card is empty on dashboard if cook choose empty option in form

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,4 @@
 class DashboardsController < ApplicationController
-
   def index
     @orders = policy_scope(Order).all
     @meals = policy_scope(Meal).all
@@ -7,11 +6,22 @@ class DashboardsController < ApplicationController
 
   def update
     @user = current_user
-    @meal = Meal.find(params[:cook][:daily_meal].to_i)
-    @user.daily_meal = @meal
+
+    if meal_id.zero?
+      @user.daily_meal = nil
+    else
+      @meal = Meal.find(params[:cook][:daily_meal].to_i)
+      @user.daily_meal = @meal
+    end
+
     authorize @user
     @user.save
     redirect_to dashboards_path
   end
 
+  private
+
+  def meal_id
+    params[:cook][:daily_meal].to_i
+  end
 end
