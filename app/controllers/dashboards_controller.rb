8fead0f189929567f374fe00f458e89b977a07e7
Tayler Johnson <johnsontayler@me.com>
2019-09-13 18:49:15 -0300

    daily meal form only displays meals choices of cook. and orders display is only orders of cooks daily meal. profile counter works now as well. woo

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,8 +2,8 @@ class DashboardsController < ApplicationController
   def index
     @orders = policy_scope(Order).all
     @meals = policy_scope(Meal).all
-    @new_orders = current_user.orders.where(completed: false)
-    @complete_orders = current_user.orders.where(completed: true)
+    @new_orders = new_orders
+    @complete_orders = complete_orders
   end
 
   def update
@@ -25,5 +25,27 @@ class DashboardsController < ApplicationController
 
   def meal_id
     params[:cook][:daily_meal].to_i
+  end
+
+  def new_orders
+    @daily_meal = current_user.daily_meal_id
+    if @daily_meal.nil?
+      @new_orders = []
+    else
+      @meal = Meal.find(@daily_meal.to_i)
+      @new_orders = @meal.orders.where(completed: false)
+    end
+    @new_orders
+  end
+
+  def complete_orders
+    @daily_meal = current_user.daily_meal_id
+    if @daily_meal.nil?
+      @complete_orders = []
+    else
+      @meal = Meal.find(@daily_meal.to_i)
+      @complete_orders = @meal.orders.where(completed: true)
+    end
+    @complete_orders
   end
 end
