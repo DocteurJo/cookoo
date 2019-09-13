384d13591e63c0d27fdcfe3e083e669227a8fa3d
Tayler Johnson <johnsontayler@me.com>
2019-09-13 13:26:14 -0300

    added update method to admin orders controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,4 +3,16 @@ class Admin::OrdersController < ApplicationController
     @cook_orders = current_user.meals.orders
     @orders = @cook_orders.sort_by(:time)
   end
+
+  def update
+    @order = Order.find(params[:id])
+    authorize @order
+    @order.update(task_params)
+  end
+
+  private
+
+  def task_params
+    arams.require(:order).permit(:time, :quantity, :completed)
+  end
 end
--- db/migrate/20190913161533_add_completed_to_orders.rb
+++ db/migrate/20190913161533_add_completed_to_orders.rb
@@ -1,5 +1,5 @@
 class AddCompletedToOrders < ActiveRecord::Migration[5.2]
   def change
-    add_column :orders, :completed, :boolean
+    add_column :orders, :completed, :boolean, default: false
   end
 end
